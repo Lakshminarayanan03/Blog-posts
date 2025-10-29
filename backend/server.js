93dd4f226ef7fe89ff2d33bb0e706fcbf40e4152
Lakshminarayanan03 <badhrinarayanan1605@gmail.com>
2025-10-30 00:22:23 +0530

    Fetch frontend file from backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 require('dotenv').config();
 
+
 const express = require('express');
 const connectDb = require('./conifg/db')
 const postRoutes = require('./routes/postRoute')
 const userRoutes = require('./routes/userRoute')
 const cors = require("cors");
+const path = require("path");
+
 
 const app = express();
 
 app.use(cors());
+
 
 const port = 3000;
 
@@ -18,6 +22,13 @@ app.use(express.json());
 app.use('/api/users', userRoutes)
 app.use('/api/posts',postRoutes);
 
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+});
 
 
 app.listen(port, (req,res) =>{
